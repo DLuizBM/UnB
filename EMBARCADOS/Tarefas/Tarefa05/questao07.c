6402907f9caf65dec296b9ecc935a7bc624301df
Nasser Yousef Ali <nasser.yousef.unb@gmail.com>
2017-03-27 23:59:41 -0300

    Terminadas as questões da aula prática 5 e feito o exercício teórico da aula 5

--- EMBARCADOS/Tarefas/Tarefa05/questao07.c
+++ EMBARCADOS/Tarefas/Tarefa05/questao07.c
@@ -7,8 +7,8 @@
 
 int main(int argc, char *argv[]){
 
-    char c, conteudo[100];
-    int fp, i = 0, j = 0; // Definindo o ponteiro p/ ler o arquivo
+    char c, conteudo[500], palavra[20];
+    int fp, i = 0, j = 0, k, l, ocorrencia = 0; // Definindo o ponteiro p/ ler o arquivo
 
     // Abre o arquivo de acordo com o 1º parâmetro que
     // foi passado, só como leitura.
@@ -21,15 +21,39 @@ int main(int argc, char *argv[]){
 
     // Reposicionao ponteiro descrito do arquivo na posição 0
     lseek(fp, 0, SEEK_SET);
+
+    while(read(fp, &c, 1) != 0){
+		conteudo[i] = c;
+		i++;
+    }
+
+    l = i;
+    i = 0;
+
+    printf("\nConteudo do arquivo: %s\n", conteudo);
+    printf("Palavra procurada: %s\n", argv[2]);
+    printf("Quantidade de caracteres do conteudo do arquivo: %d\n\n", l);
     
-    while(i <= tam_arq_texto(argv[1])){
-    	fscanf(&fp, "%s", conteudo);
-    	if(strcmp(conteudo, argv[2]) == 0)
+    while(i <= (l+20)){
+    	if(conteudo[i] != ' ' && conteudo[i] != ',' && conteudo[i] != '.' && conteudo[i] != '!'){
+    		palavra[j] = conteudo[i];
     		j++;
+    	}else{
+
+    		printf("Palavra: %s\n", palavra);
+    		if(strcmp(palavra, argv[2]) == 0){
+    			ocorrencia++;
+    		}
+
+    		// limpar a variável palavra
+    		for(k = 0; k < j; k++)
+    			palavra[k] = null;
+    		j = 0;
+    	}
     	i++;
     }
 
-    printf("\n%s ocorreu %d vezes no arquivo %s", argv[2], j, argv[1]);
+    printf("\n%s ocorreu %d vezes no arquivo %s\n\n", argv[2], ocorrencia, argv[1]);
 
     close(fp);
 
